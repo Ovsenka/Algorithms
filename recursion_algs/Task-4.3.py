d13e83883292998dbb5cfe713c0cef8aa3ecf23b
Ovsenka <ovsenka3@gmail.com>
2022-11-11 21:38:59 +0300

    ed

--- recursion_algs/Task-4.3.py
+++ recursion_algs/Task-4.3.py
@@ -1,3 +1,3 @@
-# s_num is number for searching
-# Base: len(list_) == 1: return lst[0]
+# s_num is number for searching, lst is a list of numbers
+# Base: len(lst) == 1: return lst[0]
 # recursive: return lst[0:lst.index(half)] if half >= s_num else lst[lst.index(half):lst[-1]]
