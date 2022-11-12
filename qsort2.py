e829829178e21d7a48dade96deaa03928f1b1bd2
Ovsenka <ovsenka3@gmail.com>
2022-11-13 00:34:10 +0300

    upgrade

--- qsort2.py
+++ qsort2.py
@@ -1,3 +1,4 @@
+# quicksort from book
 
 def qsort(lst: list) -> list[int]:
     if len(lst) < 2:
@@ -7,4 +8,16 @@ def qsort(lst: list) -> list[int]:
     greater = [i for i in lst[1:] if i > pivot]
     return qsort(less) + [pivot] + qsort(greater)
 
+#better
+def qsort_2(lst: list) -> list[int]:
+    if len(lst) < 2:
+        return lst 
+    pivot = len(lst) // 2
+    less = [i for i in lst if i < pivot]
+    greater = [i for i in lst if i > pivot]
+    eq = [pivot] * lst.count(pivot)
+    return qsort(less) + eq + qsort(greater)
+
+
 print(qsort([4, 3, 8, 9, 10, 11]))
+print(qsort_2([4, 3, 8, 99, 10, 11]))
